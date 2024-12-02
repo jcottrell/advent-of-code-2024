973161a3bba0de6dad591d2da0a2bbf358f96ba8
Joshua Cottrell <joshua.e.cottrell@pm.me>
2024-12-02 10:16:22 -0500

    nix flake setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "For racket development";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.racket
+          ];
+        };
+      });
+}
