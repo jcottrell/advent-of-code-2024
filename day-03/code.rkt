07a7ad4c36e666fd2c5c68d4eb3d13fceeab8023
Joshua Cottrell <joshua.e.cottrell@pm.me>
2024-12-05 17:43:41 -0500

    Finishes day 03, part 2
    I rearranged part 1 because I thought part 2's input might be so large
    that I couldn't parse it in memory and would have to stream it. It looks
    me a while to see that the input file was actually multiple lines and my
    part 1 solution was handling that well. I also realized it was good to
    test the final answer for part 1 with a check-equal? so I could be sure
    I kept getting the right answer. When I removed the map, map, map layer
    from part 1 I was effectively ignoring all but the first line of text so
    it didn't work any more. It look me a while to realize that I was
    getting part 2's answer wrong because I prematurely optimized part 1
    without checking for its correctness. I just checked that it compiled
    and worked on the simple (1 line) input. Instead of reverting to maps I
    joined all the strings, which fixed both parts and reduced the
    code (though it wouldn't work with very large inputs).
    
    Still suffers from the run-code duplication problem.

--- day-03/code.rkt
+++ day-03/code.rkt
@@ -3,29 +3,29 @@
 (require rackunit
          threading)
 
+(provide (rename-out [file->muls-totaled part1])
+         (rename-out [file->do-dont-muls-totaled part2]))
+
 ;; part one
 (define (file->muls-totaled file-name)
    (~> file-name
        (file->lines #:mode 'text)
-       lines->digit-pairs
-       (map mul-the-digit-pairs _)
        flatten
+       (string-join "")
+       line->raw-muls
+       raw-muls->string-pairs
+       string-pairs->digit-pairs
+       mul-the-digit-pairs
        (apply + _)))
 
-(define (lines->digit-pairs lines)
-  (~> lines
-      (map line->raw-muls _)
-      (map raw-muls->string-pairs _)
-      (map string-pairs->digit-pairs _)))
+(define (line->raw-muls line)
+  (regexp-match* #px"mul\\([[:digit:]]{1,3},[[:digit:]]{1,3}\\)" line))
 
 (define (raw-muls->string-pairs muls)
   (map raw-mul->string-pair muls))
 
 (define (string-pairs->digit-pairs string-pairs)
   (map string-pair->digit-pair string-pairs))
-
-(define (line->raw-muls line)
-  (regexp-match* #px"mul\\([[:digit:]]{1,3},[[:digit:]]{1,3}\\)" line))
 
 (check-equal? (line->raw-muls "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
               '("mul(2,4)" "mul(5,5)" "mul(11,8)" "mul(8,5)"))
@@ -51,5 +51,43 @@
 (check-equal? (mul-the-digit-pairs '((2 4) (5 5) (11 8) (8 5)))
               '(8 25 88 40))
 
-(check-equal? (file->muls-totaled "short-example-list.txt") 161)
-;(file->muls-totaled "input.txt")
+(check-equal? (file->muls-totaled "short-example-list1.txt") 161)
+(check-equal? (file->muls-totaled "input.txt") 173529487)
+
+;; part two
+(define (file->do-dont-muls-totaled file-name)
+    (~> file-name
+        (file->lines #:mode 'text)
+        flatten
+        (string-join "")
+        line->do-dont-and-muls
+        filter-do-donts->raw-muls
+        raw-muls->string-pairs
+        string-pairs->digit-pairs
+        mul-the-digit-pairs
+        (apply + _)))
+
+(define (line->do-dont-and-muls line)
+  (regexp-match* #px"do\\(\\)|don't\\(\\)|mul\\([[:digit:]]{1,3},[[:digit:]]{1,3}\\)" line))
+
+(check-equal? (line->do-dont-and-muls "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+              '("mul(2,4)" "don't()" "mul(5,5)" "mul(11,8)" "do()" "mul(8,5)"))
+
+(define (filter-do-donts->raw-muls do-dont-and-muls)
+  (letrec ([filter-muls (lambda (remaining do-on? result)
+                          (cond [(null? remaining) result]
+                                [else (let* ([command-or-mul (first remaining)]
+                                             [next-remaining (rest remaining)])
+                                        (cond [(equal? "do()" command-or-mul)
+                                               (filter-muls next-remaining #t result)]
+                                              [(equal? "don't()" command-or-mul)
+                                               (filter-muls next-remaining #f result)]
+                                              [do-on? (filter-muls next-remaining do-on? (append result (list command-or-mul)))]
+                                              [else (filter-muls next-remaining do-on? result)]))]))])
+    (filter-muls do-dont-and-muls #t '())))
+
+(check-equal? (filter-do-donts->raw-muls '("mul(2,4)" "don't()" "mul(5,5)" "mul(11,8)" "do()" "mul(8,5)"))
+              '("mul(2,4)" "mul(8,5)"))
+
+(check-equal? (file->do-dont-muls-totaled "short-example-list2.txt") 48)
+(check-equal? (file->do-dont-muls-totaled "input.txt") 99532691)
